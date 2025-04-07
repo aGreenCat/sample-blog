5fa43ecbf7595f6af10705a04b9d6c607a7cede9
Tedd Lee <tedd0328@gmail.com>
2025-04-06 23:37:48 -0400

    Small Style Tweaks

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,10 @@
 import Link from 'next/link';
+import Image from "next/image";
 
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-      <div className="block w-12 h-12 mx-auto mb-4 rounded-full bg-conic-180 from-gradient-3 from-0% to-gradient-4 to-100%" />
+      <Image src={"/newgc_green.png"} alt={"Green Cat Logo"} width={100} height={100} className="mx-auto mb-4" />
       <p className="text-2xl text-center dark:text-white">
         <Link href="/">{name}</Link>
       </p>
